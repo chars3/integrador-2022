23e1da77873acf6c039303b15348c56c2f6b3810
chars3 <charesjoseph@gmail.com>
2022-03-14 08:55:12 -0300

    add card despesas

--- src/components/dashBoardComponents/CardDespesas.vue
+++ src/components/dashBoardComponents/CardDespesas.vue
@@ -2,7 +2,7 @@
   <v-card class="pa-5" color="white" height="250px">
     <div class="despesas">
       <h2>Despesas</h2>
-      <p>R$ 300,00</p>
+      <p>R$ 302,00</p>
     </div>
   </v-card>
 </template>
